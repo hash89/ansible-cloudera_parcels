4714e1514babbd8ed3434d2d3eb8bb90393eb70a
Hadrien <hadrien.puissant@gmail.com>
2017-12-20 16:56:53 +0100

    Update to handle latest tag in version argument

--- cloudera_parcels.py
+++ cloudera_parcels.py
@@ -4,7 +4,7 @@ ANSIBLE_METADATA = {
     "metadata_version"  : "1.0",
     "supported_by"      : "community",
     "status"            : ["preview"],
-    "version"           : "1.0.0"
+    "version"           : "1.1.0"
 }
 
 DOCUMENTATION = """
@@ -41,39 +41,47 @@ options:
     product:
         description:
             - The name of the parcel as it appear in the Cloudera Manager server
-        required: true
+        required: true if state is not infos
     version:
         description:
-            - The version of the parcel you want to manipulate
-        required: true
+            - The version of the parcel you want to manipulate or
+            - "latest" to target the latest version available on the Cloudera Manager server
+        required: true if state is not infos
+	choices:
+	    - 'the exact version string'
+	    - latest
     state:
         description:
             - If `present`, the module will ensure parcel is downloaded or more on the cluster
             - If `distributed`, the module will ensure the parcel is distributed on all host or more
-            - If `activated`, the module will ensure the parcel is actvated 
+            - If `activated`, the module will ensure the parcel is actvated
             - If `absent`, The module will ensure the parcel is not present on the cluster
             - it will desactivate the parcel, undistributed it and remove from hosts
+            - Info will give you informations about the parcel you target
+            - Information available : product, version, stage
+            - If you do not specify product or version the module will collect informations about all parcels available
         default: present
         choices:
             - present
             - distributed
             - activated
             - absent
+            - infos
 notes:
     - check_mode not supported yet
 """
 
 EXAMPLES = """
 
-- name: Download parcel for CDH
+- name: Download parcel for latest CDH parcel (available on the Cloudera Manager)
   cloudera_parcels:
     cm_login: john
     cm_password: toto18
     api_version: 18
     cm_host: cloudera.mycompany.com
-    cluster_name: test	
+    cluster_name: test
     product: CDH
-    version: 5.13.0-1.cdh5.13.0.p0.29
+    version: latest
     state: present
 
 - name: Distribute parcel for CDH
@@ -82,7 +90,7 @@ EXAMPLES = """
     cm_password: toto18
     api_version: 18
     cm_host: cloudera.mycompany.com
-    cluster_name: test	
+    cluster_name: test
     product: CDH
     version: 5.13.0-1.cdh5.13.0.p0.29
     state: distributed
@@ -93,7 +101,7 @@ EXAMPLES = """
     cm_password: toto18
     api_version: 18
     cm_host: cloudera.mycompany.com
-    cluster_name: test	
+    cluster_name: test
     product: CDH
     version: 5.13.0-1.cdh5.13.0.p0.29
     state: activated
@@ -104,16 +112,36 @@ EXAMPLES = """
     cm_password: toto18
     api_version: 18
     cm_host: cloudera.mycompany.com
-    cluster_name: test	
+    cluster_name: test
     product: CDH
     version: 5.13.0-1.cdh5.13.0.p0.29
     state: absent
 
+- name: Retreive informations for all parcels on a cluster
+  cloudera_parcels:
+    cm_login: john
+    cm_password: toto18
+    api_version: 18
+    cm_host: cloudera.mycompany.com
+    cluster_name: test
+    state: infos
+
+- name: Retreive informations for a sspecific parcel on a cluster
+  cloudera_parcels:
+    cm_login: john
+    cm_password: toto18
+    api_version: 18
+    cm_host: cloudera.mycompany.com
+    cluster_name: test
+    product: CDH
+    version: 5.13.0-1.cdh5.13.0.p0.29
+    state: infos
 """
 
 
 import sys
 import time
+from natsort import natsorted
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -127,15 +155,20 @@ def build_module():
         "cm_password": {"required": True, "type": "str", "no_log": True},
         "cm_host": {"required": True, "type": "str"},
         "cluster_name" : {"required": True, "type": "str"},
-        "product": {"required": True, "type": "str"},
-        "version": {"required": True, "tye": "str"},
+        "product": {"required": False, "type": "str"},
+        "version": {"required": False, "tye": "str"},
         "state": {
             "default": "present",
-            "choices": ['present', 'distributed', 'activated', 'absent'],
+            "choices": ['present', 'distributed', 'activated', 'absent', 'infos'],
             "type": 'str'
         }
     }
-
+    required_if = [
+      [ "state", "present", [ "product", "version" ] ],
+      [ "state", "distributed", [ "product", "version" ] ],
+      [ "state", "activated", [ "product", "version" ] ],
+      [ "state", "absent", [ "product", "version" ] ]
+    ]
     mutually_exclusive = []
     module = AnsibleModule(
         argument_spec=fields,
@@ -146,29 +179,46 @@ def build_module():
     return module
 
 def main():
-	module = build_module()
-	choice_map = {'present': present, 'distributed': distributed, 'activated': activated, 'absent': absent}
-	params = module.params
-	has_changed = False
+    module = build_module()
+    choice_map = {'present': present, 'distributed': distributed, 'activated': activated, 'absent': absent, 'infos': infos}
+    params = module.params
+    has_changed = False
 
-	api = ApiResource(params["cm_host"], username=params["cm_login"], password=params["cm_login"], version=params["api_version"])
+    api = ApiResource(params["cm_host"], username=params["cm_login"], password=params["cm_login"], version=params["api_version"])
 
-	try:
-		cluster = api.get_cluster(params["cluster_name"])
-	except ApiException as e:
-		module.fail_json(msg="Cluster error : {0}".format(e))
+    try:
+        cluster = api.get_cluster(params["cluster_name"])
+    except ApiException as e:
+        module.fail_json(msg="Cluster error : {0}".format(e))
 
-	try:
-		parcel = cluster.get_parcel(params["product"], params["version"])
-	except ApiException as e:
-		module.fail_json(msg="Parcel error : {0}".format(e))
+    if params["product"] and params["version"]:
+        parcel = get_parcel(cluster, params["product"], params["version"])
+        if params["state"] != "infos":
+            error, has_changed, result, meta = choice_map.get(params['state'])(cluster, parcel)
 
-	error, has_changed, result, meta = choice_map.get(params['state'])(cluster, parcel)
+            if error:
+                module.fail_json(msg=result)
+            module.exit_json(changed=has_changed, msg=result, meta=meta)
+        else:
+            meta = {
+                "product": parcel.product,
+                "version": parcel.version,
+                "stage": parcel.stage
+            }
+            module.exit_json(changed=False, msg="Parcel informations gathered", meta=meta)
+    elif not params["product"] and not params["version"] and params["state"] == "infos":
+        module.exit_json(changed=has_changed, msg="Parcel informations gathered", meta=infos(cluster))
 
-	if error:
-		module.fail_json(msg=result)
-	module.exit_json(changed=has_changed, msg=result, meta=meta)
-
+def get_parcel(cluster, product, version):
+    if version == "latest":
+        versions = []
+        for parcel in cluster.get_all_parcels():
+            versions.append(parcel.version)
+        version = natsorted(versions)[-1]
+    try:
+        return cluster.get_parcel(product, version)
+    except ApiException as e:
+        module.fail_json(msg="Parcel error : {0}".format(e))
 
 def format_meta_parcel(parcel):
 	return {
@@ -177,6 +227,15 @@ def format_meta_parcel(parcel):
 		"stage": parcel.stage
 	}
 
+def infos(cluster):
+    parcel_list = []
+    for parcel in cluster.get_all_parcels():
+        parcel_list.append({
+            "version": parcel.version,
+            "product": parcel.product,
+            "stage": parcel.stage
+        })
+    return parcel_list
 
 def distributed(cluster, parcel):
 	changed = False
@@ -185,7 +244,7 @@ def distributed(cluster, parcel):
 
 	if parcel.stage == "DOWNLOADED":
 		if parcel.start_distribution().wait(3000).success:
-			while cluster.get_parcel(parcel.product, parcel.version).stage == "DOWNLOADING":
+			while cluster.get_parcel(parcel.product, parcel.version).stage == "DISTRIBUTING":
 				time.sleep(1)
 				pass
 			changed = True
@@ -281,7 +340,7 @@ def present(cluster, parcel):
 		msg = "Parcel in transient state = {0}".format(parcel.stage)
 	else:
 		msg = "Parcel already present"
-	
+
 	return error, changed, msg, format_meta_parcel(cluster.get_parcel(parcel.product, parcel.version))
 
 
